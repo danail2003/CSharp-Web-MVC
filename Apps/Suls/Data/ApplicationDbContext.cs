135a7a0f61ab46026aede534c340acf22e03c973
danail dobrev <ddobrev21@gmail.com>
2021-05-03 10:53:01 +0300

    InitializingDbContext

---
+++ Apps/Suls/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Suls.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-LM6SUBI\SQLEXPRESS;Database=Suls;Integrated Security=true;");
+            }
+        }
+    }
+}
